5c138dd6d2f6510290b6caf434990528094508dc
woodenbridgewu <woodenbridgewu@gmail.com>
2023-03-16 16:49:04 +0800

    加了一個清空購物車的按鈕，但依舊不實用，慢慢修改！

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -9,26 +9,45 @@ function ShoppingCart() {
     setCartItems(items);
   }, []);
 
+  const clearCart = () => {
+    setCartItems([]);
+    localStorage.removeItem("cartItems");
+  };
+
   return (
     <div className={styles["cart-container"]}>
-      <table className={styles["cart-table"]}>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Quantity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems.map((item) => (
-            <tr key={item.id}>
-              <td data-label="Title">{item.title}</td>
-              <td data-label="Price">{item.price}</td>
-              <td data-label="Quantity">{item.quantity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {cartItems.length > 0 ? (
+        <>
+          <table className={styles["cart-table"]}>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((item) => (
+                <tr key={item.id}>
+                  <td data-label="Title">{item.title}</td>
+                  <td data-label="Price">{item.price}</td>
+                  <td data-label="Quantity">{item.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button
+            className={`${styles["clear-button"]} ${
+              cartItems.length === 0 && styles["hidden"]
+            }`}
+            onClick={clearCart}
+          >
+            Clear Cart
+          </button>
+        </>
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
     </div>
   );
 }
